221dd0498478fa3c15ca96ef47e5e7fa9eeee752
AnsharShampoo <anshar@ciencias.unam.mx>
2021-09-24 00:23:27 -0500

    second

--- CSES/Sorting and Searching/concert_tickets.cpp
+++ CSES/Sorting and Searching/concert_tickets.cpp
@@ -10,24 +10,6 @@ using namespace std;
 #define vif vector<int> v(n);for (int i = 0; i < n; i++) cin >> v[i]
 #define endl '\n'
 int32_t main() {
-    int tickets, ppl;
-    vector<pair<int, int>> guys;
-    vi tick; 
-    for (int i = 0; i<tickets; i++){
-        cin>>tick[i];
-    }
-    for (int i = 0; i<ppl; i++){
-        cin>>guys[i].first;
-        guys[i].second = i;
-    }
-    sort(guys.begin(),guys.end());
-    sort(tick.begin(), tick.end());
-    int a, b, flag = false;
-    while (!flag){
-        
-    }
-    
-    
     return 0;
-
+    int 
 }
