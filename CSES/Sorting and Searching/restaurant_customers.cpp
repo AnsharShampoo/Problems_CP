a706579d853991f47001ca8fd25606e2e31f77b2
AnsharShampoo <anshar@ciencias.unam.mx>
2021-10-07 20:50:59 -0500

    Training 07/Oct/2021

---
+++ CSES/Sorting and Searching/restaurant_customers.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define int long long
+#define vi vector<int>
+#define ii pair<int, int>
+#define vii vector<pair<int, int>>
+#define vvi vector<vector<int>>
+#define vif vector<int> v(n);for (int i = 0; i < n; i++) cin >> v[i]
+#define endl '\n'
+int32_t main() {
+    int n;
+    cin>>n;
+    vector<pair<int, int>> hours;
+    for (int i = 0; i<n; i++){
+        int a, b;
+        cin>>a>>b;
+     hours.push_back(make_pair(a,b));
+    }
+    sort(hours.begin(), hours.end());
+    int count = 1;
+    int max = 0;
+    for (int i = 1; i<n; i++){
+        if (hours[i].second <= hours[i-1].first){
+            count+=1;
+            if (count>max){
+                max = count;
+            }
+        }
+        else{
+            count = 0;
+        }
+    }
+    cout<<max;
+
+}
+
